983c2ef97b5189da0f3e2715a88ceea3819357c7
SKY-20190828GFP\Administrator <daannii371@gmail.com>
2021-06-20 01:14:12 +0900

    re-make(ing-)

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems" :key=todoItem class="shadow">
+      <li v-for="(todoItem, index) in todoItems" :key=todoItem class="shadow">
         <i class="checkBtn fas fa-check" type="button" aria-hidden="ture"></i>
         {{ todoItem }}
         <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
@@ -18,17 +18,52 @@ export default {
     return {
       todoItems: []
     }
+    
   },
+
   created() {
     if(localStorage.length > 0) {
       for(var i=0; i < localStorage.length; i++){
         this.todoItems.push(localStorage.key(i));
       }
     }
+  },
+  methods: {
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+      }
+    },
+  
   }
 
-}
+
 </script>
 
-<style>
+<style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+  }
+  li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+  .checkBtn {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+  }
+  .removeBtn {
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
